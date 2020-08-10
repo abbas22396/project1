bd9e08779e351decc803cf7fd0476d84e0994d1f
abbas22396 <abbasstudy22@gmail.com>
2020-08-10 12:18:52 +0530

    My first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+        <footer classname="w-100 bg-light text-center">
+            <p>All Rights Reserved</p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
